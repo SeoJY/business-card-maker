40cab07c14a67d977391f8515379681d899d6dd1
서재영 <aldeazures@hivelab.co.kr>
2022-06-19 19:08:15 +0900

    feat: 이미지 로더처리

--- src/components/maker/inputs/imgFileInput.jsx
+++ src/components/maker/inputs/imgFileInput.jsx
@@ -1,23 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from './button';
 
 const ImgFileInput = ({ imageUploader, name, onFileChange }) => {
-  const inputRef = useRef();
+  const [loading, setLoading] = useState(false);
 
+  const inputRef = useRef();
   const onFileUpload = event => {
     event.preventDefault();
     inputRef.current.click();
   };
 
   const onChange = async event => {
+
+    setLoading(true);
     const uploadedFile = event.target.files[0];
     const uploaded = await imageUploader.upload(uploadedFile);
     const fileName = uploadedFile['name'];
-
+    
+    
     onFileChange({
       name: fileName,
       url: uploaded.url,
     })
+    
+    setLoading(false);
   };
 
   return (
@@ -30,7 +36,8 @@ const ImgFileInput = ({ imageUploader, name, onFileChange }) => {
         className="blind"
         onChange={onChange}
       />
-      <Button content={name || 'Photo Upload'} size="medium" color="grey" isfull onClickFunc={onFileUpload} />
+      {!loading && <Button content={name || 'Photo Upload'} size="medium" color={name ? 'pink' : 'grey'} isfull onClickFunc={onFileUpload} />}
+      {loading && <div className="loader"></div> }
     </div>
   );
 };
